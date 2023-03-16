445ee75d72fce8ba6107a137f5c1ee03ef91f38d
aero <44799877+AeroWang@users.noreply.github.com>
2023-03-16 20:55:10 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default async function Page() {
             target="_blank"
             rel="noreferrer"
           >
-            leerob
+            aero
           </a>{' '}
           / <Link href="/">on-demand-isr</Link>
         </div>
